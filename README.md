dcfcfad7e008e8d5814e92c36b1c59fa1b908d1f
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-21 15:26:49 -0400

    Added and tested SWR for data fetching

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # SuperHero API
+
+## Resources
+
+-   SWR, implemented based on making it a reusable function -- https://swr.vercel.app/docs/getting-started#make-it-reusable
--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import FetchData from './utils/FetchData';
 import "./App.module.scss";
 
 function App() {
+    // Get vars from FetchData and check for errors / availability of feed
+    const { data, isLoading, isError } = FetchData();
+    if (isError) return <div>Failed to load</div>;
+    if (isLoading) return <div>Loading...</div>;
+
+    console.log(data);
+
     return (
         <div>
             Hello World
