a1eafde472f50ba9be9b096e9e43571c857add8e
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-21 15:07:41 -0400

    Removed fiels from fork, created and cleaned up react app, updated favicon and logos and switched out for scss modules

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.module.scss";
+
+function App() {
+    return (
+        <div>
+            Hello World
+        </div>
+    );
+}
+
+export default App;
