7ebfc9b400a478cc08a36e2e507da602b5031055
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-23 17:12:28 -0400

    Styled powers inside overlay

--- src/components/HeroPowers/HeroPowers.js
+++ src/components/HeroPowers/HeroPowers.js
@@ -8,11 +8,11 @@ const HeroPowers = (props) => {
         <div className={classes.herocard__overlay}>
             <ul className={classes.herocard__powers}>
                 <li><span>{combat}</span>Combat</li>
-                <li><span>{durability}</span>Combat</li>
-                <li><span>{intelligence}</span>Combat</li>
-                <li><span>{power}</span>Combat</li>
-                <li><span>{speed}</span>Combat</li>
-                <li><span>{strength}</span>Combat</li>
+                <li><span>{durability}</span>Durability</li>
+                <li><span>{intelligence}</span>Intelligence</li>
+                <li><span>{power}</span>Power</li>
+                <li><span>{speed}</span>Speed</li>
+                <li><span>{strength}</span>Strength</li>
             </ul>
         </div>
     )
