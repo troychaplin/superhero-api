2b6ee83290eb1211057bf81a4c645c7f7d2cf90a
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-23 00:05:18 -0400

    Change alignment indicator style, added helper function to uppercase first letter of a string

--- src/components/HeroCard/HeroCard.js
+++ src/components/HeroCard/HeroCard.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from '../../ui/Card/Card';
+import { FirstLetterUpper } from '../../utils/Helper';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSuperpowers } from "@fortawesome/free-brands-svg-icons";
 import classes from './HeroCard.module.scss';
@@ -10,8 +11,6 @@ const HeroCard = (props) => {
     const { race } = appearance;
     const noData = 'No data available';
 
-    console.log(alignment);
-
     return (
         <Card className={`${classes["hero-card"]} ${classes[alignment]}`}>
             <div className={classes["image-wrapper"]} style={{backgroundImage: `url('${images.sm}')`}} />
@@ -21,7 +20,7 @@ const HeroCard = (props) => {
                 <ul>
                     <li><strong>Real Name:</strong> {fullName || noData}</li>
                     <li><strong>Race:</strong> {race || noData}</li>
-                    <li><strong>Alignment:</strong> {alignment || noData}</li>
+                    <li><strong>Alignment:</strong> {FirstLetterUpper(alignment) || noData}</li>
                     <li><strong>Publisher:</strong> {publisher || noData}</li>
                 </ul>
             </div>
