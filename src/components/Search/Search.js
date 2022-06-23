f7fb8db59c634319a9cd9a166649645056c7c3fc
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-23 12:03:59 -0400

    Updated search input a11y and tested with tenon.io

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -5,12 +5,21 @@ function Search(props) {
     // Capture the input and set as props
     const searchHandler = (event) => {
         props.onSearchInput(event.target.value);
-    }
+    };
 
     return (
         <form className={classes.search}>
             <label htmlFor='search'>Search for a hero</label>
-            <input id='search' type='text' onChange={searchHandler} autoComplete='name' placeholder='Search for a hero' />
+            <input
+                id='search'
+                type='text'
+                aria-label='Search for a hero'
+                aria-required='true'
+                name='Hero Search'
+                onChange={searchHandler}
+                autoComplete='name'
+                placeholder='Search for a hero'
+            />
         </form>
     );
 }
