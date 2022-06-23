1ec838d1ed10698fbae2da7e79325393797c5f3b
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-23 16:37:49 -0400

    Setup basics for styling overlay

--- src/components/HeroPowers/HeroPowers.js
+++ src/components/HeroPowers/HeroPowers.js
@@ -1,10 +1,16 @@
-// import React from "react";
-// import classes from './HeroPowers.module.scss';
+import React from "react";
+import classes from './HeroPowers.module.scss';
 
 const HeroPowers = (props) => {
     console.log(props.heroPowers);
 
-    // return <p>HeroPowers</p>
+    return (
+        <div className={classes.herocard__overlay}>
+            <ul>
+                <li><span>{props.heroPowers.combat}</span>Combat</li>
+            </ul>
+        </div>
+    )
 }
 
 export default HeroPowers;
