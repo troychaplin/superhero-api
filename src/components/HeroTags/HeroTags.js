474ec2af0a8ddc6dba52c592d81d15c44c3888c8
Troy Chaplin <troy.chaplin@gmail.com>
2022-06-24 00:20:58 -0400

    Added HeroTag component

---
+++ src/components/HeroTags/HeroTags.js
@@ -0,0 +1,8 @@
+import React from 'react';
+// import classes from './HeroTags.module.scss';
+
+const HeroTags = () => {
+    return <p>HeroTags</p>;
+};
+
+export default HeroTags;
--- src/components/_template/Template.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Template.module.scss';
-
-const Template = (props) => {
-    return <p>Template</p>;
-};
-
-export default Template;
